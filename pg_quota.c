ca5852601118434415441215719608ce82b2738b
RekGRpth <rekgrpth@gmail.com>
2024-10-12 13:55:20 +0500

    -main

--- pg_quota.c
+++ pg_quota.c
@@ -5,23 +5,23 @@
 #include <postmaster/bgworker.h>
 #include <utils/guc.h>
 
-PGDLLEXPORT void pg_quota_launcher_main(Datum main_arg);
+PGDLLEXPORT void pg_quota_launcher(Datum arg);
 void _PG_init(void);
 
-static int pg_quota_launcher_main_restart;
+static int pg_quota_launcher_restart;
 
 static void pg_quota_init(bool dynamic) {
     BackgroundWorker worker = {0};
     size_t len;
     elog(DEBUG1, "dynamic = %s", dynamic ? "true" : "false");
-    if ((len = strlcpy(worker.bgw_function_name, "pg_quota_launcher_main", sizeof(worker.bgw_function_name))) >= sizeof(worker.bgw_function_name)) ereport(ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("strlcpy %li >= %li", len, sizeof(worker.bgw_function_name))));
+    if ((len = strlcpy(worker.bgw_function_name, "pg_quota_launcher", sizeof(worker.bgw_function_name))) >= sizeof(worker.bgw_function_name)) ereport(ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("strlcpy %li >= %li", len, sizeof(worker.bgw_function_name))));
     if ((len = strlcpy(worker.bgw_library_name, "pg_quota", sizeof(worker.bgw_library_name))) >= sizeof(worker.bgw_library_name)) ereport(ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("strlcpy %li >= %li", len, sizeof(worker.bgw_library_name))));
     if ((len = strlcpy(worker.bgw_name, "pg_quota launcher", sizeof(worker.bgw_name))) >= sizeof(worker.bgw_name)) ereport(WARNING, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("strlcpy %li >= %li", len, sizeof(worker.bgw_name))));
 #if PG_VERSION_NUM >= 110000
     if ((len = strlcpy(worker.bgw_type, worker.bgw_name, sizeof(worker.bgw_type))) >= sizeof(worker.bgw_type)) ereport(ERROR, (errcode(ERRCODE_OUT_OF_MEMORY), errmsg("strlcpy %li >= %li", len, sizeof(worker.bgw_type))));
 #endif
     worker.bgw_flags = BGWORKER_SHMEM_ACCESS | BGWORKER_BACKEND_DATABASE_CONNECTION;
-    worker.bgw_restart_time = pg_quota_launcher_main_restart;
+    worker.bgw_restart_time = pg_quota_launcher_restart;
     worker.bgw_start_time = BgWorkerStart_RecoveryFinished;
     if (dynamic) {
         worker.bgw_notify_pid = MyProcPid;
@@ -33,10 +33,10 @@ static void pg_quota_init(bool dynamic) {
 
 void _PG_init(void) {
     if (!process_shared_preload_libraries_in_progress) ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("This module can only be loaded via shared_preload_libraries")));
-    DefineCustomIntVariable("pg_quota.launcher_main_restart", "pg_quota launcher main restart", "Restart launcher main interval, seconds", &pg_quota_launcher_main_restart, BGW_DEFAULT_RESTART_INTERVAL, 1, INT_MAX, PGC_SUSET, 0, NULL, NULL, NULL);
+    DefineCustomIntVariable("pg_quota.launcher_restart", "pg_quota launcher restart", "Restart launcher interval, seconds", &pg_quota_launcher_restart, BGW_DEFAULT_RESTART_INTERVAL, 1, INT_MAX, PGC_SUSET, 0, NULL, NULL, NULL);
     pg_quota_init(false);
 }
 
-void pg_quota_launcher_main(Datum main_arg) {
+void pg_quota_launcher(Datum arg) {
 
 }
